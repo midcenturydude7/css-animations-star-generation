6b911fb871c543ed7419cf49d8de640ebcf5d829
midCenturyDude7 <mgriffes@gmail.com>
2025-10-02 20:13:21 -0700

    Completed project

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 import { random, range } from "lodash";
+
+const btn = document.querySelector(".particleButton");
+const starLayer = document.querySelector(".starLayer");
+
+btn.addEventListener("click", () => {
+  range(10).forEach(() => {
+    const star = document.createElement("div");
+    star.classList.add("star");
+    star.innerText = "⭐";
+
+    star.style.top = `${random(0, 100)}%`;
+    star.style.left = `${random(0, 100)}%`;
+
+    starLayer.appendChild(star);
+  });
+});
